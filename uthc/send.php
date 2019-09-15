640d66e6796eed49f22926bea648584507c56973
Gustavo <gguerrerom@vera.com.uy>
2019-09-14 21:23:42 -0300

    2019_09_14 Version
    Se corrrige el problema del viewport en  index.html

--- uthc/send.php
+++ uthc/send.php
@@ -6,6 +6,6 @@
     $mensaje = $_POST["mensaje"];
     $contenido = "Nombre: " . $nombre . "\nE-Mail: " . $email . "\nCelular: " . $celular . "\nMensaje: " . $mensaje ;
     mail($destino,"Contacto", $contenido);
-    header("location:gracias.html");
+    header("Location:gracias.html");
     
 ?>
